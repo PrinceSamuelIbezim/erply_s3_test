3c1310aa3a3f8b131f98d0e1957da397934bbacb
Macbook <macbook@Macbooks-MBP.lan>
2024-05-19 20:12:46 +0300

    first commit

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -13,7 +13,7 @@ terraform {
 
   backend "s3" {
     bucket         = "erply-bucket-to-storestate-for-my-test-task"
-    key            = "terraform/s3_website/deployment.tfstate"
+    key            = "terraform/s3_website/state.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
